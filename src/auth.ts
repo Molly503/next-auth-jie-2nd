7821acc7e7222d57744fcf81abd78d07314b1c30
Jie Liang <liangjie_wm@163.com>
2024-06-17 19:13:48 +0800

    button

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,13 @@
-import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "./lib/prisma";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import Github from "next-auth/providers/github";
+import prisma from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    logo: "/logo.png",
+  },
   adapter: PrismaAdapter(prisma),
-  providers: [Google, Github],
+  providers: [Google, GitHub],
 });
